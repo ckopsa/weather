885f1a873553700ff8a2e6a0dd7ab4180290eef1
coljamkop <coljamkop@gmail.com>
2017-06-07 13:06:09 -0600

    Removed personal filepath

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
 import requests, bs4, sys
+
+weatherFilePath = "./weather.txt"
 url = "https://weather.com/weather/today/l/"
 zip = sys.argv[1]
 ext = ":4:US"
@@ -8,7 +9,7 @@ res = requests.get(url + zip + ext)
 res.raise_for_status();
 weatherSoup = bs4.BeautifulSoup(res.text, "html.parser")
 weatherCard = weatherSoup.find("div", { "class" : "today_nowcard-temp" })
-weatherFile = open('/home/aghnix/.config/polybar/weather.txt', 'r+')
+weatherFile = open(weatherFilePath, 'r+')
 if weatherCard is not None:
     temp = weatherCard.select('span')[0].getText()
     weather = weatherSoup.find("div", { "class" : "today_nowcard-phrase" }).getText()
